cd86dd001383e6406bae524262231e13c86c4b15
arindam <creativeab26@gmail.com>
2021-09-11 14:19:24 +0530

    made the post and postSection (ui) part complete

--- src/app/Containers/PostSection.jsx
+++ src/app/Containers/PostSection.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import Post from "../components/Post";
 
 const PostSection = () => {
+    //TODO: remove the ugly scrollbar and make visible full post
   return (
-    <div className="max-w-7xl mx-auto h-full overflow-x-hidden flex justify-center my-5">
+    <div className="max-w-7xl mx-auto h-full overflow-y-scroll flex flex-col items-center my-5">
+      <Post />
+      <Post />
       <Post />
     </div>
   );
--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -1,10 +1,11 @@
 import { Avatar, Title, Text, Image } from "@mantine/core";
 import React from "react";
-import { AiFillLike, AiOutlineComment, AiOutlineBook } from "react-icons/ai";
+import { BiLike, BiShareAlt } from "react-icons/bi";
+import { FaRegComment, FaRegBookmark } from "react-icons/fa";
 
 const Post = () => {
   return (
-    <div className="w-3/6 h-2/5 bg-white rounded-md px-5  py-7">
+    <div className="w-3/6 h-3/5 bg-white rounded-md px-5 my-4 py-7">
       <div className="flex space-x-3">
         <Avatar radius={"lg"} />
         <div>
@@ -13,13 +14,28 @@ const Post = () => {
         </div>
       </div>
 
-      <Image className="my-2" src="/screenshot.png" fit="contain" />
+      <Image className="my-4" src="/screenshot.png" fit="contain" />
       <div className="flex justify-between">
         <div className="flex space-x-5">
-          <AiFillLike className="text-3xl" />
-          <AiOutlineComment className="text-3xl" />
+          <BiLike className="text-3xl" />
+          <FaRegComment className="text-3xl" />
+          <BiShareAlt className={"text-3xl"} />
         </div>
-        <AiOutlineBook className="text-3xl" />
+        <FaRegBookmark className="text-3xl" />
+      </div>
+
+      <div className={"flex items-center my-2"}>
+        <Avatar radius={"sm"} />
+        <Text>
+          Liked by <span className={"font-bold"}>anidita007</span>,and{" "}
+          <span className={"font-bold"}>1993</span> others
+        </Text>
+      </div>
+      <div>
+          <Text>
+              <span className={"font-bold mr-2"}>Allu0076</span>
+              what garbage is this .it's so bad
+          </Text>
       </div>
     </div>
   );
